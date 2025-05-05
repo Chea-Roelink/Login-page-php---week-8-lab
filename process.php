18962fedfe57cf95ee11f4585dc2b745bcce22aa
Jinger010010 <129271245+Jinger010010@users.noreply.github.com>
2025-05-05 13:25:10 +1000

    yep

--- process.php
+++ process.php
@@ -1,1 +1,13 @@
+<?php
+session_start();
+$username = $_SESSION["username"];
+$password = $_SESSION["password"];
+
+if ($username == "admin" && $password == "password123") {
+    $_SESSION["user"] = $username;
+    header("Location: welcome.php");
+} else {
+    echo "Invalid Login. <a href='login.html'>Try again?<a/>";
+}
+?>
 
--- welcome.php
+++ welcome.php
@@ -1,1 +1,16 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+    echo"Welcome, " . $_SESSION["user"];
+} else {
+    header("Location: login.html");
+}
+?>
 
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php include 'header.inc'; ?>
+        <?php include 'footer.inc'; ?>
+    </body>
+</html>
